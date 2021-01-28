66f322a4b01f73cf031c079d20fb63069d856923
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-01-28 16:22:33 -0500

    complete

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,7 +5,7 @@ class UserSerializer < ActiveModel::Serializer
 
   def videos
     self.object.videos.order(:id).map do |video| 
-      {id: video.id, city_id: video.city_id, user_id: video.user_id, title: video.title, category: video.category, video_url: video.video_url, likes: video.likes}
+      {id: video.id, city_id: video.city_id, city: video.city.name, user_id: video.user_id, title: video.title, category: video.category, video_url: video.video_url, likes: video.likes}
     end
   end
 
--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,10 +1,14 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :city_id, :user_id, :title, :category, :video_url, :likes, :uploader, :comments
+  attributes :id, :city_id, :city, :user_id, :title, :category, :video_url, :likes, :uploader, :comments
 
   def comments
     self.object.comments.order(:id).map do |comment| 
       {id: comment.id, video_id: comment.video_id, user_id: comment.user_id, author: comment.author, comment: comment.comment}
     end
+  end
+
+  def city
+    self.object.city.name
   end
 
 end
